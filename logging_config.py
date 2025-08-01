af9799c6ffbd33d979fe78598efe809aec3ddcd6
Maria Petersen <mau.petersen@gmail.com>
2025-08-01 19:25:13 +0200

    add log file

--- logging_config.py
+++ logging_config.py
@@ -2,7 +2,7 @@ import logging
 
 def setup_logging():
     logging.basicConfig(
-        filename='/home/pi/pump.log',  # Log file location
+        filename='pump.log',  # Log file location
         level=logging.INFO,            # Minimum logging level
         format='%(asctime)s %(levelname)s: %(message)s'  # Log format
     )
